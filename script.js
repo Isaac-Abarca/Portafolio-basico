36bb5b2fa19f51d8e4ff00fc070aaef51a73653c
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-06-13 12:02:53 -0600

    Added new project and added modal for the img

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+function openModal(src) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src = src;
+
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  modal.onclick = function() {
+      modal.style.display = "none";
+  }
+}
